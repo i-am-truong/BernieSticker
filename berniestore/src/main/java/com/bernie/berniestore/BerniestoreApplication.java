96a26b463295601478e2f61a4a360a8685141da1
Bernie Truong <tqtolympia@gmail.com>
2025-07-04 09:32:16 +0700

    init project

---
+++ berniestore/src/main/java/com/bernie/berniestore/BerniestoreApplication.java
@@ -0,0 +1,13 @@
+package com.bernie.berniestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerniestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BerniestoreApplication.class, args);
+	}
+
+}
